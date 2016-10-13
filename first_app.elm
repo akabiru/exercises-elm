319b5370251094015256ddb8ebaa6913a8464fef
Austin Kabiru <austin.kabiru@andela.com>
2016-10-13 17:42:49 +0300

    upto first app

---
+++ first_app.elm
@@ -0,0 +1,81 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Html.App as App
+
+
+-- Model
+-- Initial state of the app
+
+
+type alias Model =
+    Int
+
+
+initModel : Model
+initModel =
+    0
+
+
+
+-- Update
+
+
+type Msg
+    = AddCalorie
+    | RemoveCalorie
+    | Clear
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        AddCalorie ->
+            model + 1
+
+        RemoveCalorie ->
+          if model > 0 then
+            model - 1
+          else
+            initModel
+
+        Clear ->
+            initModel
+
+
+
+-- View
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ h3 []
+            [ text ("Total Calories: " ++ (toString model)) ]
+        , button
+            [ type' "button"
+            , onClick AddCalorie
+            ]
+            [ text "Add" ]
+        , button
+            [ type' "button"
+            , onClick RemoveCalorie
+            ]
+            [ text "Remove" ]
+        , button
+            [ type' "button"
+            , onClick Clear
+            ]
+            [ text "Clear" ]
+        ]
+
+
+main : Program Never
+main =
+    App.beginnerProgram
+        { model = initModel
+        , update = update
+        , view = view
+        }
